4d8a1316df430fb2f9984f8e6c6cf81c11b9ff85
Nisaja26 <nissaikaracmawati@gmail.com>
2024-10-10 16:06:31 +0700

    template 03 100%

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,18 @@ Route::get('/template', function () {
 Route::get('/', function () {
     return view('admin.layouts.wrapper');
 });
+
+Route::get('/user', function () {
+    $data = [
+        'content' => 'admin.user.index' // perbaiki tanda panah menjadi =>
+    ];
+    return view('admin.layouts.wrapper', $data);
+});
+
+Route::get('/post', function () {
+    $data = [
+        'content' => 'admin.post.index' // perbaiki tanda panah menjadi =>
+    ];
+    return view('admin.layouts.wrapper', $data);
+});
+
